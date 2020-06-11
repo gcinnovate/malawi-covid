c59d151d7b2f923a8f2d204eecf3a4e79136fa98
sekiskylink <sekiskylink@gmail.com>
2020-06-11 16:50:06 +0300

    fix notifications, drop redis usage

--- app/api/notifications.py
+++ app/api/notifications.py
@@ -1,15 +1,16 @@
 from flask import jsonify, request
 from . import api
-from .. import redis_client, CASE_MESSAGE_TEMPLATE, SUSPECTS_MESSAGE_TEMPLATE
+from .. import CASE_MESSAGE_TEMPLATE, SUSPECTS_MESSAGE_TEMPLATE
 from string import Template
 from .tasks import send_sms_notification
+from ..models import NotifyingParties, Location
 
 
 @api.route('/notify', methods=['GET'])
 def notify():
     # districts = redis_client.districts
-    rapid_response_team = redis_client.rapid_response_team
-    global_notifying_parties = redis_client.global_notifying_parties
+    # rapid_response_team = redis_client.rapid_response_team
+    # global_notifying_parties = redis_client.global_notifying_parties
     name = request.args.get('name', '')
     district = request.args.get('district', '')
     phone = request.args.get('phone', '')
@@ -26,8 +27,17 @@ def notify():
             "has been confirmed as a COVID-19 case ")
     message = Template(msg_template).safe_substitute({
         'name': name, 'phone': phone, 'district': district, 'address': address})
-    recipients = rapid_response_team.get(district)
-    recipients.extend(global_notifying_parties)
+
+    rapid_response_team = NotifyingParties.query.filter_by(
+            reporter_type='rrt',
+            district=Location.query.filter_by(name=district).first()).with_entities('msisdn').all()
+    global_notifying_parties = NotifyingParties.query.filter_by(
+            reporter_type='phim').with_entities('msisdn').all()
+
+    recipients = [i[0] for i in rapid_response_team]
+    recipients_2 = [i[0] for i in global_notifying_parties]
+
+    recipients.extend(recipients_2)
     print(recipients, message)
-    send_sms_notification.delay(message, recipients)
+    # send_sms_notification.delay(message, recipients)
     return jsonify({'message': 'success'})
